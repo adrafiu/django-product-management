e9c74203b548db393bacc940544fec9fda8a280e
Md. Abdul Rafeu <rafiu.diu@gmail.com>
2026-09-22 21:04:57 +0600

    Added update and delete button

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name= 'home'),
     path('add_product/', views.add_product, name= 'add_product'),
     path('product_list/', views.product_list, name= 'product_list'),
+    path('update_product/<int:product_id>/', views.update_product, name='update_product'),
+    path('delete_product/<int:product_id>/', views.delete_product, name='delete_product')   
 
 ]    
--- products/views.py
+++ products/views.py
@@ -28,9 +28,14 @@ def add_product(request):
 
 def product_list(request):
     products = ProductModel.objects.all()
-    
+
+    filtered_products = ProductModel.objects.filter(
+        price__gte=100
+    )
+
     context = {
-        "products": products
+        "products": products,
+        "filtered_products": filtered_products
     }
 
     return render(
@@ -38,3 +43,44 @@ def product_list(request):
         "product_list.html",
         context
     )
+
+
+def update_product(request, product_id):
+    product = ProductModel.objects.get(id=product_id)
+
+    if request.method == "POST":
+        product.name = request.POST.get("name")
+        product.description = request.POST.get("description")
+        product.price = request.POST.get("price")
+        product.production_date = request.POST.get("production_date")
+
+        if request.FILES.get("image"):
+            product.image = request.FILES.get("image")
+
+        product.save()
+
+        return redirect("product_list")
+
+    products = ProductModel.objects.all()
+
+    filtered_products = ProductModel.objects.filter(
+        price__gte=100
+    )
+
+    context = {
+        "products": products,
+        "filtered_products": filtered_products,
+        "product": product,
+        "edit_mode": True
+    }
+
+    return render(
+        request,
+        "product_list.html",
+        context
+    )
+    
+def delete_product(request, product_id):
+    product = ProductModel.objects.get(id=product_id)
+    product.delete()
+    return redirect("product_list")    
